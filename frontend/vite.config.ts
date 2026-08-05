84ff1557ec7f0863a12be40784f69b693e82fd25
jack <jackharper0517@gmail.com>
2026-08-05 17:10:32 -0500

    updated packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
+  build: {
+    commonjsOptions: {
+      include: [/react-router-dom/, /node_modules/],
+    },
   },
 });
